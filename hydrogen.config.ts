b0d16e1d1470f177b8c55232037b01f3703f8551
wromo <info.wromo@gmail.com>
2022-07-18 10:18:06 +0200

    add ifyshop complet connect shopify mit ,, yarn run preview ''

---
+++ hydrogen.config.ts
@@ -0,0 +1,18 @@
+import {defineConfig, CookieSessionStorage} from '@shopify/hydrogen/config';
+
+export default defineConfig({
+  shopify: {
+    defaultCountryCode: 'US',
+    defaultLanguageCode: 'EN',
+    storeDomain: 'next-ifyshop.myshopify.com',
+    storefrontToken: '334b4b062c287a6ca349e42bd0cc659e',
+    storefrontApiVersion: '2022-07',
+  },
+  session: CookieSessionStorage('__session', {
+    path: '/',
+    httpOnly: true,
+    secure: import.meta.env.PROD,
+    sameSite: 'Strict',
+    maxAge: 60 * 60 * 24 * 30,
+  }),
+});
